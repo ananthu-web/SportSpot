2aaa092d81bd9d5ead9783b5b1ce0b1485596b6f
ananthu-web <myelectonicmailjr11@gmail.com>
2025-12-10 13:14:41 +0530

    s

--- backend/Server.js
+++ backend/Server.js
@@ -40,8 +40,8 @@ const startServer = async () => {
     await seedSportsData(); 
     await seedCourtData() 
 
-    const PORT = process.env.PORT;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT ,"0.0.0.0", () => console.log(`Server running on port ${PORT}`));
   } catch (err) {
     console.error("Server startup error:", err);
   }
