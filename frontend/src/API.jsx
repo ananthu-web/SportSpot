2538674c2470fd2dc53534a34bef6a5e3a0a1040
ananthu-web <myelectonicmailjr11@gmail.com>
2025-12-15 11:45:28 +0530

    lat

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -1,17 +1,17 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:3000", 
-});
-
-
-export default API;
-
-
 // import axios from "axios";
 
 // const API = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL, // uses .env variable
+//   baseURL: "http://localhost:3000", 
 // });
 
+
 // export default API;
+
+
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL, // uses .env variable
+});
+
+export default API;
