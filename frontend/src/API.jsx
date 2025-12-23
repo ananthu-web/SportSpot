1d88baa9b80e153c8535bb6ea35478e96f305d2a
ananthu-web <myelectonicmailjr11@gmail.com>
2025-12-23 15:23:15 +0530

    api

--- frontend/src/API.jsx
+++ frontend/src/API.jsx
@@ -3,8 +3,17 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL ||
-    "http://localhost:3000",
+    import.meta.env.VITE_API_BASE_URL ,
 });
 
 export default API;
+
+
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL:
+//     "http://localhost:3000",
+// });
+
+// export default API;
